9e16eb1d13099052fe9b1025ac0e0551bc926a68
MrKOSMOS <berniegraz@gmail.com>
2021-12-02 19:05:10 +0000

    Update emu.cc

--- src/ui/SDL2/gui_modules/emu.cc
+++ src/ui/SDL2/gui_modules/emu.cc
@@ -284,13 +284,13 @@ void EmuModule::update() {
 }
 
 void EmuModule::output() {
-/^  // output audio!
+/*  // output audio!
   float* samples = nullptr;
   uint   count = 0;
   this->gui.nes.getAudiobuff(&samples, &count);
   // SDL_QueueAudio(this->gui.sdl.nes_audiodev, samples, count * sizeof(float));
   if (count) this->sdl.sound_queue.write(samples, count);
-^/
+*/
   // output video!
   const u8* framebuffer;
   this->gui.nes.getFramebuff(&framebuffer);
